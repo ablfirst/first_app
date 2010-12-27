d672de6d313860fcec462debbf45156a39a2d41c
abl <abl@traen.com>
2010-12-27 21:29:39 +0100

    xx

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '342facb2e8ce81f697ce088036dc73124856fc3ec1cd5768a9aab8daa361234671d0c4f69fb08a4c9c5d4c2659a6ead7e91a8dc81ecc49d78e2c70be981d0f54'
+FirstApp::Application.config.secret_token = '9bebb5df7f54f469f33a85f456d6cc67a19184e2f5d82178a24d89cb563f88401774ff2fa9ec4c83d9d6999df706ecb8b736f646cfaf2187bafe6459aa7a740e'
